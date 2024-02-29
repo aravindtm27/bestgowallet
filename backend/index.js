2d1d47b2a765142d39a8a6aab3fec4a1ea43d699
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2024-02-29 05:54:11 +0530

    First Commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,27 @@
+
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const apiRoute = require("./src/routes/index.js")
+const userRoute = require("./src/routes/user.js");
+const accountRoute = require("./src/routes/account.js");
+const {authMiddleware} = require("./src/middleware/middleware.js")
 
 
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect("mongodb+srv://aravindtm98:aravindtm98@cluster0.8b7vdei.mongodb.net/bestgowallet").then((e)=>console.log("Mongodb connected"));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use("/api/v1/wallet",apiRoute);
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/account",accountRoute);
+
+app.listen(PORT, ()=>console.log("server started at port 4000"));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landingPage/landingPage";
+import Dashboard from "./pages/dashboard/dashboard";
+import Signup from "./pages/signup/signup";
+import Signin from "./pages/signin/signin";
+import Send from "./pages/send/send";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<LandingPage />}/>
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='signin' element={<Signin />} />
+            <Route path='send' element={<Send />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
